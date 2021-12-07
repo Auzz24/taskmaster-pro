02c8aaa31f95907b0be7f1ef2b20adfa77fc459f
Austin Miller <austin.miller1424@gmail.com>
2021-12-06 20:00:51 -0600

    edit task/date

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -1,4 +1,3 @@
-//test
 var tasks = {};
 
 var createTask = function(taskText, taskDate, taskList) {
@@ -73,14 +72,95 @@ $("#task-form-modal .btn-primary").click(function() {
     // close modal
     $("#task-form-modal").modal("hide");
 
+   
     // save in tasks array
     tasks.toDo.push({
-      text: taskText,
-      date: taskDate
-    });
+    text: taskText,
+    date: taskDate
+      });
+
 
     saveTasks();
   }
+});
+
+$(".list-group").on("click", "p", function (){
+  var text = $(this)
+  .text()
+  .trim();
+
+  var textInput = $("<textarea>").addClass("form-control").val(text);
+  $(this).replaceWith(textInput);
+
+  textInput.trigger("focus")
+});
+
+// editable field was un-focused
+$(".list-group").on("blur", "textarea", function() {
+  // get current value of textarea
+  var text = $(this).val();
+  
+// get the parent ul's id attribute
+var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+tasks[status][index].text= text;
+saveTasks();
+
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with new content
+  $(this).replaceWith(taskP);
+});
+
+// due date was clicked
+$(".list-group").on("click", "span", function() {
+  // get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+  $(this).replaceWith(dateInput);
+
+  // automatically bring up the calendar
+  dateInput.trigger("focus");
+});
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function() {
+  var date = $(this).val();
+
+  // get status type and position in the list
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span and insert in place of input element
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+    $(this).replaceWith(taskSpan);
 });
 
 // remove all tasks
@@ -94,5 +174,3 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
-
-
